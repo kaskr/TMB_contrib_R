fa94ea56659cc0cf43381296564b96724734d3ef
Jim Thorson <JamesT.esq@gmail.com>
2016-06-24 09:56:26 -0700

    added TMBhelper...
    .... which for now has a simpler calculation for AIC

---
+++ TMBhelper/R/TMBAIC.R
@@ -0,0 +1,16 @@
+
+#' Calculate marginal AIC for a fitted model
+#'
+#' \code{TMBAIC} calculates AIC for a given model fit
+#'
+#' @param opt, the output from \code{nlminb} or \code{optim}
+#' @param k the penalty on additional fixed effects (default=2, for AIC)
+#'
+#' @return AIC, where a parsimonious model has a AIC relative to other candidate models
+
+TMBAIC=function(opt, k=2){
+  if( all(c("par","objective") %in% names(opt)) ) Return = 2*length(opt[["par"]]) + k*opt[["objective"]]
+  if( all(c("par","value") %in% names(opt)) ) Return = 2*length(opt[["par"]]) + k*opt[["value"]]
+  return( Return )
+}
+
---
+++ TMBhelper/README.md
@@ -0,0 +1,2 @@
+# TMB-debug
+Debugger for windows machines when running Template Model Builder, developed by Kasper Kristensen
