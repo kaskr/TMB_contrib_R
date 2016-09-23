8d47b2e0ef38fd6a4e901d0dbe6cbc247d3c7c61
Jim Thorson <JamesT.esq@gmail.com>
2016-09-23 07:22:15 -0700

    changed notation for TMBAIC, and ...
    .. added sample size input for using AICc

--- TMBhelper/R/TMBAIC.R
+++ TMBhelper/R/TMBAIC.R
@@ -3,15 +3,18 @@
 #'
 #' \code{TMBAIC} calculates AIC for a given model fit
 #'
-#' @param opt, the output from \code{nlminb} or \code{optim}
-#' @param k the penalty on additional fixed effects (default=2, for AIC)
+#' @param opt the output from \code{nlminb} or \code{optim}
+#' @param p the penalty on additional fixed effects (default=2, for AIC)
+#' @param n the sample size, for use in AICc calculation (default=Inf, for which AICc=AIC)
 #'
 #' @return AIC, where a parsimonious model has a AIC relative to other candidate models
 
 #' @export
-TMBAIC=function(opt, k=2){
-  if( all(c("par","objective") %in% names(opt)) ) Return = k*length(opt[["par"]]) + 2*opt[["objective"]]
-  if( all(c("par","value") %in% names(opt)) ) Return = k*length(opt[["par"]]) + 2*opt[["value"]]
+TMBAIC=function(opt, p=2, n=Inf){
+  k = length(opt[["par"]])
+  if( all(c("par","objective") %in% names(opt)) ) negloglike = opt[["objective"]]
+  if( all(c("par","value") %in% names(opt)) ) negloglike = opt[["value"]]
+  Return = p*k + 2*negloglike + 2*k*(k+1)/(n-k-1)
   return( Return )
 }
 
