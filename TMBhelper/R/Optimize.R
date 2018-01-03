4ebc0d7013b3f2af577e24aebc84e8166c8c4c5b
colemonnahan <monnahc@uw.edu>
2018-01-03 17:58:11 -0300

    Add mgc to output of Optimizer

--- TMBhelper/R/Optimize.R
+++ TMBhelper/R/Optimize.R
@@ -44,6 +44,7 @@ Optimize = function( obj, fn=obj$fn, gr=obj$gr, startpar=obj$par, lower=rep(-Inf
 
   # Add diagnostics
   opt[["run_time"]] = Sys.time() - start_time
+  opt[["max_gradient"]] = max(abs(obj$gr(opt$par)))
   opt[["number_of_coefficients"]] = c("Total"=length(unlist(obj$env$parameters)), "Fixed"=length(obj$par), "Random"=length(unlist(obj$env$parameters))-length(obj$par) )
   opt[["AIC"]] = TMBhelper::TMBAIC( opt=opt )
   if( n!=Inf ){
